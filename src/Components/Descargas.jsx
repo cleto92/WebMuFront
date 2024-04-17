64a8a402382029eedfd8b46fa3e33981691923d0
cleto92 <estebanquitoet@gmail.com>
2024-04-17 10:50:40 -0300

    Modificacion Descargas

--- src/Components/Descargas.jsx
+++ src/Components/Descargas.jsx
@@ -23,7 +23,10 @@ const Descargas = () => {
   return (
     <div>
       <NavbarItem className="bg-transparent text-white font-bold">
-        <Link className="cursor-pointer text-lg hover:text-blue-300 transition duration-300 ease-in-out" onClick={onOpenDescargas}>
+        <Link
+          className="cursor-pointer text-lg hover:text-blue-300 transition duration-300 ease-in-out"
+          onClick={onOpenDescargas}
+        >
           DESCARGAS
         </Link>
         <Modal
@@ -45,7 +48,15 @@ const Descargas = () => {
                   <Card className="mt-4 shadow-md rounded-lg">
                     <CardBody className="bg-gray-50 p-4 rounded-lg shadow-sm">
                       <div className="flex flex-col gap-2">
-                        <p className="font-semibold">Descargar: <Link href="https://www.mediafire.com/file/nz9ik3ltaj6ev5y/Cliente_Club_5.exe/file"  className="text-blue-500 hover:underline">MEDIAFIRE</Link></p>
+                        <p className="font-semibold">
+                          Descargar:{" "}
+                          <Link
+                            href="https://www.mediafire.com/file/nz9ik3ltaj6ev5y/Cliente_Club_5.exe/file"
+                            className="text-blue-500 hover:underline"
+                          >
+                            MEDIAFIRE
+                          </Link>
+                        </p>
                         <div className="font-medium text-gray-700">
                           <p>Requisitos:</p>
                           <ul className="list-disc pl-5">
@@ -66,16 +77,45 @@ const Descargas = () => {
                   <Card className="mt-4 shadow-md rounded-lg">
                     <CardBody className="bg-gray-50 p-4 rounded-lg shadow-sm">
                       <div className="flex flex-col gap-2">
-                        <p className="font-semibold">Descargar: <Link href="https://mega.nz/file/2wQlQJ5S#2nuN1LRaWYcJU4YFojUI8sbXA7rwGI4lh9j2jLws5a8"  className="text-blue-500 hover:underline">MEGA</Link></p>
+                        <p className="font-semibold">
+                          Descargar:{" "}
+                          <Link
+                            href="https://mega.nz/file/2wQlQJ5S#2nuN1LRaWYcJU4YFojUI8sbXA7rwGI4lh9j2jLws5a8"
+                            className="text-blue-500 hover:underline"
+                          >
+                            MEGA
+                          </Link>
+                        </p>
                         <div className="font-medium text-gray-700">
                           <p>Requisitos:</p>
                           <ul className="list-disc pl-5">
-                          <li>Procesador: 2 Núcleos o más AMD/INTEL</li>
+                            <li>Procesador: 2 Núcleos o más AMD/INTEL</li>
                             <li>RAM: 1 GB</li>
                             <li>Placa de Video 256 MB</li>
                             <li>Conexión a internet 10 MB o más</li>
                           </ul>
                         </div>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Tab>
+                <Tab
+                  title="PARCHE"
+                  className="font-semibold text-gray-700 hover:text-blue-500"
+                >
+                  <Card className="mt-4 shadow-md rounded-lg">
+                    <CardBody className="bg-gray-50 p-4 rounded-lg shadow-sm">
+                      <div className="flex flex-col gap-2">
+                        <p className="font-semibold">
+                          Descargar:{" "}
+                          <Link
+                            href="#"
+                            className="text-blue-500 hover:underline"
+                          >
+                            PARCHE_V2
+                          </Link>
+                        </p>
+                        <div className="font-medium text-gray-700"></div>
                       </div>
                     </CardBody>
                   </Card>
@@ -91,4 +131,3 @@ const Descargas = () => {
 };
 
 export default Descargas;
-
